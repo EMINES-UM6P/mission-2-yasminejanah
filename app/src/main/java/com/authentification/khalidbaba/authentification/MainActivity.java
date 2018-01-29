57b6aa404a36b14fb5135ed2aec6cdd1e7f242f8
yasmine.janah <yasminejanah@icloud.com>
2018-01-29 18:15:24 +0000

    mission 2

--- app/src/main/java/com/authentification/khalidbaba/authentification/MainActivity.java
+++ app/src/main/java/com/authentification/khalidbaba/authentification/MainActivity.java
@@ -1,24 +1,44 @@
 package com.authentification.khalidbaba.authentification;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by khalidbaba on 28/01/2018.
  */
 
 
+
 public class MainActivity extends Activity {
-
-
+    Button Login;
+    EditText Email;
+    EditText Password;
+    String _email;
     // TODO : declare all variables (elements AND variables)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    Login=(Button)findViewById(R.id.btnLogin);
+    Email=(EditText)findViewById(R.id.email);
+    Password=(EditText)findViewById(R.id.password);
+    Login.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+        _email=Email.getText().toString();
+            Log.i("btnlogin","button clicked");
+            Intent i=new Intent(MainActivity.this,WolcomeActivity.class);
+            i.putExtra("Email",_email);
+            startActivity(i);
+        }
+    });
 
         //TODO Instantiate object (EditText and Button)
 
@@ -33,3 +53,4 @@ public class MainActivity extends Activity {
 
     }
 }
+
--- app/src/main/java/com/authentification/khalidbaba/authentification/WolcomeActivity.java
+++ app/src/main/java/com/authentification/khalidbaba/authentification/WolcomeActivity.java
@@ -1,8 +1,10 @@
 package com.authentification.khalidbaba.authentification;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 /**
  * Created by khalidbaba on 28/01/2018.
@@ -11,15 +13,21 @@ import android.support.annotation.Nullable;
 public class WolcomeActivity extends Activity {
 
     //TODO : Declare all variables (TextView, String)
+    TextView Email;
+    String _email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
+        Intent i=getIntent();
+        _email=i.getStringExtra("Email");
+        Email=(TextView)findViewById(R.id.email);
+        Email.setText(_email);
         //TODO : Instantiate all objects
 
 
         //TODO  : Get the intent content and show the Email in the TextView
     }
 }
+
